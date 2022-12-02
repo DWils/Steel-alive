005f29768c6522ba9477d689ff846d256a0b231a
Remar59 <remlevy@hotmail.fr>
2022-12-02 09:13:57 +0100

    rem

---
+++ javascript/recherche.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    
+    var checkBox = document.getElementById("hideapple","hidelenovo");
+   
+    var text = document.getElementById("macbookair-m1","lenovo_thinkpad");
+    
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  } 
+  function myFunctionz() {
+    
+    var checkBox = document.getElementById("hideapple","hidelenovo");
+   
+    var text = document.getElementById("macbookair-m1","lenovo_thinkpad");
+    
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  } 
